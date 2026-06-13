bed5ed15cc5d25ac9605f45ca426ad5c23dbf10f
mdsahilcoder-cmd <mdsahilcoder@gmail.com>
2026-06-13 15:36:47 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
+from telegram import Update
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
+import os
+TOKEN = os.getenv("8632052379:AAH95t70Ib3AHuZnQCQdISYFf0RmLVXpP0Y")"
+
+async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+    
+    await update.message.reply_text(
+        f"Message mila: {user_message}\nBot ka backend kaam kar raha hai ✅"
+    )
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(MessageHandler(filters.TEXT, reply))
+
+print("Bot running...")
+
+app.run_polling()
